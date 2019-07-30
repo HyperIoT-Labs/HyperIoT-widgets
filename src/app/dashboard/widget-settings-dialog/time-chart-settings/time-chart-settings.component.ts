00f2429320f797933a7182b676f0fcf4f8524365
Gene <generoso.martello@acsoftware.it>
2019-07-30 17:56:21 +0200

    fix bug in time-chart settings dialog

--- src/app/dashboard/widget-settings-dialog/time-chart-settings/time-chart-settings.component.ts
+++ src/app/dashboard/widget-settings-dialog/time-chart-settings/time-chart-settings.component.ts
@@ -13,8 +13,72 @@ export class TimeChartSettingsComponent implements OnInit, OnDestroy {
   @Input() modalApply: Subject<any>;
   @Input() widget;
   seriesTitle = 'Untitled';
+  private defaultConfig = {
+    timeAxisRange: 10,
+    maxDataPoints: 100,
+    timeWindow: 60,
+    seriesConfig: [
+      {
+          series: 'temperature',
+          config: {
+              yaxis: 'y2'
+          },
+          layout: {
+              title: {
+                  font: {
+                      size: 14,
+                      color: '#16A4FA'
+                  },
+                  xref: 'container',
+                  yref: 'container',
+                  x: 0,
+                  y: 1,
+                  pad: {
+                      t: 10,
+                      l: 10
+                  },
+                  text: '<b>Real time temperature and humidity</b>'
+              },
+              yaxis2: {
+                  title: 'temperature',
+                  titlefont: {
+                      color: '#00f'
+                  },
+                  tickfont: {
+                      color: '#00f'
+                  },
+                  anchor: 'free',
+                  overlaying: 'y',
+                  side: 'right',
+                  position: 1,
+                  range: [
+                      -50,
+                      50
+                  ]
+              }
+          }
+      },
+      {
+          series: 'humidity',
+          layout: {
+              yaxis: {
+                  title: 'humidity',
+                  titlefont: {
+                      color: 'darkorange'
+                  },
+                  tickfont: {
+                      color: 'darkorange'
+                  }
+              }
+          }
+      }
+  ]};
+
 
   ngOnInit() {
+    if (this.widget.config.seriesConfig == null) {
+      Object.assign(this.widget.config, this.defaultConfig);
+    }
     this.seriesTitle = this.widget.config.seriesConfig[0].layout.title.text;
     this.modalApply.subscribe((event) => {
         if (event === 'apply') {
