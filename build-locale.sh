11c1c7233c3962ed017794dc3bc4552a1b16d69e
Gene <generoso.martello@acsoftware.it>
2019-08-30 11:26:42 +0200

    splitted translatrion files

---
+++ build-locale.sh
@@ -0,0 +1,10 @@
+./node_modules/xlf-merge/index.js \
+    src/locale/widgets/*.en-US.xlf \
+    src/locale/dashboard/*.en-US.xlf \
+    -o src/locale/translations.en-US.xlf
+
+./node_modules/xlf-merge/index.js \
+    src/locale/widgets/*.it-IT.xlf \
+    src/locale/dashboard/*.it-IT.xlf \
+    -o src/locale/translations.it-IT.xlf
+
